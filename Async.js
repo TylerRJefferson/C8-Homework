ad04000a2b13be48fb305f78be341cd0d8649fae
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-06 17:13:34 -0400

    HW upload

---
+++ Async.js
@@ -0,0 +1,3 @@
+const fs = require("fs")
+const data = fs.readFileSync("Tyler.txt" , "utf8")
+console.log(data)
---
+++ day4-homework.js
@@ -0,0 +1,29 @@
+// Write a loop that outputs
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// BLAST OFF!
+
+
+function countDownStart (num){
+  
+  for(let i = num; i >= 1; i--) {
+    console.log(i); {
+      if (i === 1)
+      console.log('BLAST OFF!')
+    }
+  }
+  
+}
+countDownStart(20)
+
+
+
+
