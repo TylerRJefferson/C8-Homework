fb9cb2b97cb758b18706d9d5540d3ed2902df6f9
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-17 17:29:18 -0400

    new hw

---
+++ w3d1-homework.js
@@ -0,0 +1,27 @@
+/*
+You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored. 
+Create a function that calculates the final points for the team and returns that value.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+EXTRA CHALLENGE: Can you find the needle in the haystack?
+Write a function findNeedle() that takes an array full of items but containing one item, "needle".
+After your function finds the needle, it should return a message (as a string) that says:
+"found the needle at position " plus the index it found the needle.
+
+Example(Input --> Output)
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] => "found the needle at position 5"
+*/
